aa71ca863465b79ec82584cd29d06484a0b57d8d
master <master@gmail.com>
2026-04-24 17:51:55 +0500

    Third commit

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(body: Center(child: Title('A', HitType.hit))),
+      home: Scaffold(body: Center(child: Title('Cancel', HitType.hit))),
     );
   }
 }
