b7ea519bd74b7c311972446ef00d6820992be928
master <master@stud.com>
2026-04-29 16:40:49 +0500

    29.04.2026

---
+++ main.dart
@@ -0,0 +1,71 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'game.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class Title extends StatelessWidget {
+  const Title(this.letter, this.hitType, {super.key});
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: const Color.fromARGB(255, 14, 141, 141)),
+        color: switch (hitType) {
+          HitType.hit => const Color.fromARGB(255, 255, 167, 189),
+          HitType.miss => Colors.grey,
+          HitType.partial => Colors.yellow,
+          _ => const Color.fromARGB(255, 167, 255, 248),
+        },
+      ),
+      child: Center(child: Text(letter)),
+    );
+  }
+}
+
+class GamePage extends StatelessWidget {
+  GamePage({super.key});
+  final Game _game = Game();
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 0.5,
+        children: [for (final guess in _game.guesses) 
+        Row(
+          children: [
+            for (final x in guess)
+            Title  (x.char, x.type)],
+          )],
+      ),
+    );
+  }
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(child: GamePage()),
+        appBar: AppBar(
+          title: Align(
+            alignment: Alignment.centerLeft,
+            child: Text("Aiganym and Zhansaya"),
+          ),
+        ),
+      ),
+    );
+  }
+}
